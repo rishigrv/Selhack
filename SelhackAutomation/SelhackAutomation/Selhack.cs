2abf4f09d2324f39180e03ef4d6ebb9f43d37818
Rishi Gaurav <rishigrv@gmail.com>
2020-10-28 19:19:50 +0530

    Added App Project

---
+++ SelhackAutomation/SelhackAutomation/Selhack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelhackAutomation
+{
+    public class Selhack
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Hi from Main");
+        }
+    }
+}
